46a13fea6dcd9d10d2709e39016b010db49af88f
chenyou <chenyouhuang@gmail.com>
2023-10-10 02:50:37 -0400

    Make buttons don't adjust with container size

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -21,36 +21,42 @@ export default function Pagination({
   handleClickNext,
 }: PaginationProps) {
   return (
-    <div className="centerRow pagination">
-      <PaginationButton
-        handleClick={handleClickPrev}
-        isDisabled={currentPage === 1}
-      >
-        &#60;
-      </PaginationButton>
-      {paginationRange?.map((page: number | string, index: number) => {
-        if (typeof page !== "string") {
-          return (
-            <PaginationButton
-              key={index}
-              selected={currentPage === page}
-              handleClick={() => {
-                handleClickPage(page);
-              }}
-            >
-              {page}
-            </PaginationButton>
-          );
-        } else {
-          return <span key={index}>...</span>;
-        }
-      })}
-      <PaginationButton
-        isDisabled={currentPage === totalPages}
-        handleClick={handleClickNext}
-      >
-        &#62;
-      </PaginationButton>
+    <div className="center">
+      <div className="pagination">
+        <PaginationButton
+          handleClick={handleClickPrev}
+          isDisabled={currentPage === 1}
+        >
+          &#60;
+        </PaginationButton>
+        {paginationRange?.map((page: number | string, index: number) => {
+          if (typeof page !== "string") {
+            return (
+              <PaginationButton
+                key={index}
+                selected={currentPage === page}
+                handleClick={() => {
+                  handleClickPage(page);
+                }}
+              >
+                {page}
+              </PaginationButton>
+            );
+          } else {
+            return (
+              <span key={index} className="pagination__dots">
+                ...
+              </span>
+            );
+          }
+        })}
+        <PaginationButton
+          isDisabled={currentPage === totalPages}
+          handleClick={handleClickNext}
+        >
+          &#62;
+        </PaginationButton>
+      </div>
     </div>
   );
 }
